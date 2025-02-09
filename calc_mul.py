de2bba14b3ff0ef1aea8496af4ea914d590f79e3
sekkeironkadaiyou <miyata.daisei.li@tut.ac.jp>
2025-02-09 17:04:38 +0900

    Fixed calc function and added test cases

--- calc_mul.py
+++ calc_mul.py
@@ -5,11 +5,11 @@ import re
 def calc(A,B):
         ai=str(A)
         bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
+        p = re.compile('\d+$')
+        if p.match(ai) and p.match(bi):
+                a=int(ai)
+                b=int(bi)
+                if 1<=a<=999 and 1<=b<=999:
                         valid=True
                 else:
                         valid=False
