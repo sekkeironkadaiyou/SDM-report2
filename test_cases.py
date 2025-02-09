cd1dae409056be991821ec37dae4f4ad7ee3a5c4
sekkeironkadaiyou <miyata.daisei.li@tut.ac.jp>
2025-02-09 23:33:55 +0900

    Update test_cases.py

--- test_cases.py
+++ test_cases.py
@@ -17,5 +17,34 @@ class TestCalc (unittest.TestCase):
                 self.assertEqual (-1, calc('a','b'))
 
         def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+                self.assertEqual (-1, calc(0.5,999))
 
+        def test_sample5 (self):
+                self.assertEqual (1, calc(1,1))
+
+        def test_sample6 (self):
+                self.assertEqual (998001, calc(999,999))
+
+        def test_sample7 (self):
+                self.assertEqual (-1, calc(0,0))
+
+        def test_sample8 (self):
+                self.assertEqual (-1, calc(1000,1000))
+
+        def test_sample9 (self):
+                self.assertEqual (-1, calc(-1,999))
+
+        def test_sample10 (self):
+                self.assertEqual (-1, calc(5,2000))
+
+        def test_sample11 (self):
+                self.assertEqual (-1, calc('B','998'))
+
+        def test_sample12 (self):
+                self.assertEqual (-1, calc(0.123,500))
+
+        def test_sample13 (self):
+                self.assertEqual (-1, calc(1,None))
+
+        def test_sample14 (self):
+                self.assertEqual (-1, calc(20,'a1'))
